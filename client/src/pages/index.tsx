ac163ae4feccf71ee805b90c3a6e4028cd4b4031
felgueres <pablofelgueres@gmail.com>
2024-05-05 09:45:16 -0700

    support 2 scenes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [query, setQuery] = useState('');
-  const [imageSrc, setImageSrc] = useState('/scenes/shack/base-top.png');
+  const [scene, setScene] = useState('shack');
+  const [imageSrc, setImageSrc] = useState(`/scenes/${scene}/base-top.png`);
   const [imageIndex, setImageIndex] = useState(0);
   const [imageList, setImageList] = useState([]);
 
@@ -13,26 +14,33 @@ export default function Home() {
     const interval = setInterval(() => {
       if (imageList.length > 1) {
         setImageIndex((prevIndex) => (prevIndex + 1) % imageList.length);
-        setImageSrc(`/scenes/shack/${imageList[(imageIndex + 1) % imageList.length]}`);
+        setImageSrc(`/scenes/${scene}/${imageList[imageIndex]}`);
       }
-    }, 3000); // Change image every 3 seconds
+    }, 1000);
 
     return () => clearInterval(interval);
-  }, [imageList, imageIndex]);
+  }, [imageList, imageIndex, scene]);
 
   const updateImageSrc = (input: string) => {
     setQuery(input);
     const inputLower = input.trim().toLowerCase();
-    const allImages = {
-      'can': ['can-side.png'],
-      'chair': ['chair-side.png', 'chair-top01.png'],
-      'candle': ['candle-side.png', 'candle-top01.png'],
-      'chips': ['chips-side.png', 'chips-top01.png'],
-      'cup': ['cup-side.png', 'cup-top01.png'],
-      'oreo': ['oreo-side.png', 'oreo-top01.png'],
-      'wallet': ['wallet-side.png', 'wallet-top01.png'],
+    const scenes = {
+      'shack': {
+        'bottle': ['can-side.png'],
+        'chair': ['chair-side.png', 'chair-top01.png'],
+        'candle': ['candle-side.png', 'candle-top01.png'],
+        'chips': ['chips-side.png', 'chips-top01.png'],
+        'cup': ['cup-side.png', 'cup-top01.png'],
+        'oreo': ['oreo-side.png', 'oreo-top01.png'],
+        'wallet': ['wallet-side.png', 'wallet-top01.png'],
+      },
+      'greece': {
+        'candle': ['candle-side.png', 'candle-top01.png'],
+        'chips': ['chips-side.png', 'chips-top01.png'],
+      }
     };
 
+    const allImages = scenes[scene] || {};
     const matchedImages = Object.entries(allImages).reduce((acc, [key, values]) => {
       if (inputLower.startsWith(key)) {
         acc.push(...values);
@@ -42,26 +50,33 @@ export default function Home() {
 
     if (matchedImages.length > 0) {
       setImageList(matchedImages);
-      setImageSrc(`/scenes/shack/${matchedImages[0]}`); // Set the first matching image initially
+      setImageSrc(`/scenes/${scene}/${matchedImages[0]}`);
     } else {
       setImageList([]);
-      setImageSrc('/scenes/shack/base-side.png'); // Fallback to base.jpg if no match
+      setImageSrc(`/scenes/${scene}/base-side.png`);
     }
+  };
+
+  const changeScene = (newScene: string) => {
+    setScene(newScene);
+    setImageSrc(`/scenes/${newScene}/base-side.png`);
+    setImageList([]);
+    setImageIndex(0);
   };
 
   return (
     <main className={`flex gap-3 min-h-screen flex-col items-center justify-between p-12 ${inter.className} w-full max-w-[840px] mx-auto`}>
       <div className="flex flex-row gap-3 items-center justify-between w-full">
-        <h1 className="flex font-medium text-4xl font-mono"> 3D Search Engine </h1>
+        <h1 className="flex font-medium text-4xl font-mono">3D Search Engine</h1>
         <div className="flex gap-3">
-          <div className="border px-5 py-3 border-black rounded-md font-mono">Scene 1</div>
-          <div className="border px-5 py-3 border-black rounded-md font-mono">Scene 2</div>
+          <button className="border px-5 py-3 border-black rounded-md font-mono" onClick={() => changeScene('shack')}>Scene 1</button>
+          <button className="border px-5 py-3 border-black rounded-md font-mono" onClick={() => changeScene('house')}>Scene 2</button>
         </div>
       </div>
       <div className="flex gap-5">
         <div>
           {imageSrc && (
-            <div className="border border-gray-500 max-w-[740px] w-full  rounded-md">
+            <div className="border border-gray-500 max-w-[740px] w-full rounded-md">
               <img src={imageSrc} width={"100%"} alt="Searched item" className="max-w-full h-auto" />
             </div>
           )}
@@ -75,6 +90,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </main >
+    </main>
   );
 }
